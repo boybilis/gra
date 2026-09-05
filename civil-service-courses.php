ade1eedc8ef48708325096a03ffc3992b9b276b8
boybilis <josepharamil@gmail.com>
2026-09-05 10:51:55 +0800

    Fix CSE package card widths

--- civil-service-courses.php
+++ civil-service-courses.php
@@ -223,8 +223,8 @@
         <p>Complete Civil Service Exam test preparation in one package.</p>
       </div>
       <div class="container">
-        <div class="row justify-content-center gy-4">
-          <div class="col-6" data-aos="fade-up" data-aos-delay="100">
+        <div class="row justify-content-center gy-4 civil-service-package-grid">
+          <div class="col-6 civil-service-package-column" data-aos="fade-up" data-aos-delay="100">
             <div class="service-item position-relative h-100 civil-service-package-card">
               <div class="icon"><i class="fas fa-graduation-cap"></i></div>
               <h3>Complete Test Prep Package</h3>
@@ -240,7 +240,7 @@
               <h2 class="civil-service-package-price"><strong>Price: ₱3,500</strong></h2>
             </div>
           </div>
-          <div class="col-6" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-6 civil-service-package-column" data-aos="fade-up" data-aos-delay="200">
             <div class="service-item position-relative h-100 civil-service-package-card">
               <div class="icon"><i class="fas fa-graduation-cap"></i></div>
               <h3>Final Coaching Test Prep Package</h3>
